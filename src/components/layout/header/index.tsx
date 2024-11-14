d4d1fc47d8498feae8023d5528ecc413af7a5a0a
Eduardo <corl.facturacion@gmail.com>
2024-11-14 11:17:14 -0600

    feat- add styles & refactor footer

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./header.module.scss";
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__logo}>
+        <img src="/src/assets/svg/logo.svg" alt="logo" />
+      </div>
+      <div className={styles.header__content}>
+        <h1>A history of everything you copy</h1>
+        <p>
+          Clipboard allows you to track and organize everything you copy.
+          Instantly access your clipboard on all your devices.
+        </p>
+      </div>
+    </header>
+  );
+};
