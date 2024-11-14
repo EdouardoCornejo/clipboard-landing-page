94c497d440bbc999eb56851cdbd8d4f6d2adfee0
Eduardo <corl.facturacion@gmail.com>
2024-11-14 11:18:36 -0600

    feat- add download-btn figma component

---
+++ src/components/downloadButtons/index.tsx
@@ -0,0 +1,10 @@
+import { Button } from "../form";
+import styles from "./download-btn.module.scss";
+ 
+export const DownloadBtn = () => {
+    return ( 
+    <div className={styles.btnContainer}>
+        <Button color="primary" text="Download for iOS" />
+        <Button color="secondary" text="Download for Mac" />
+    </div> );
+}
