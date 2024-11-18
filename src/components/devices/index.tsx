34364f258770721eeb962722bf69b90ce09f4c55
Eduardo <corl.facturacion@gmail.com>
2024-11-18 10:24:39 -0600

    feat-add new Devices section

---
+++ src/components/devices/index.tsx
@@ -0,0 +1,9 @@
+import styles from './devices.module.scss';
+
+export const Devices = () => {
+  return (
+    <div className={styles.devices}>
+      <img src="/src/assets/png/image-devices.png" alt="" />
+    </div>
+  );
+};
